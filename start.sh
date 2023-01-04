fa734005764a388234e65aaa5ce942c56a166075
Alberto Moratilla <alberto.moratilla@gmail.com>
2023-01-04 22:11:22 +0100

    Add gitignore and update start script with new port

--- start.sh
+++ start.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-echo "Open your browser and use:"
-echo "http://localhost:5000/hello"
+echo "*** Open your browser and use:"
+echo "- http://localhost:80/hello"
+echo "- http://localhost:80/"
 
 
 
 echo "Starting FLASK server"
 export FLASK_ENV=development
-FLASK_APP=run.py pipenv run flask run
+FLASK_APP=run.py pipenv run flask run --host 0.0.0.0 --port 80
 
 
